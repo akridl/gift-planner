c13cd2b31266c2f4f03dd8745e2ff78fb9758f79
Dominika Blehova <dominika.blehova@bluesoft.cz>
2024-12-03 21:34:22 +0100

    fix: remove incorrect export from page components

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 import { LoginForm } from './login-form';
 
-export const LoginPage = () => (
+const LoginPage = () => (
 	<main className="flex items-center justify-center md:h-screen">
 		<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
 			<LoginForm />
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,6 +1,6 @@
 import { RegisterForm } from './register-form';
 
-export const RegisterPage = () => (
+const RegisterPage = () => (
 	<main className="flex items-center justify-center md:h-screen">
 		<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
 			<RegisterForm />
