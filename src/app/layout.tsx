51b8f90d6245673aa01404997a4fd3315b54b839
Dominika Blehova <dominika.blehova@bluesoft.cz>
2024-12-17 11:09:21 +0100

    feat: add SEO metadata to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,30 @@ import { auth } from '@/auth';
 const poppins = Poppins({ subsets: ['latin'], weight: ['400'] });
 
 export const metadata: Metadata = {
-	title: 'Gift planner'
+	title: 'Gift Planner',
+	description:
+		'Plan, organize, and share your gift ideas effortlessly with friends using our Gift Planner app.',
+	keywords:
+		'Gift planner, gift ideas, organize gifts, share gifts with friends, group gift planning, buy gifts for friends',
+	authors: [
+		{ name: 'Dominika Blehová' },
+		{ name: 'Vít Nakládal' },
+		{ name: 'Adam Krídl' }
+	],
+	openGraph: {
+		title: 'Gift Planner - Plan, Organize, and Share Your Perfect Gifts',
+		description:
+			'Collaborate on gift ideas and stay organized with your group.',
+		type: 'website',
+		url: 'https://gift-planner-pv247.vercel.app/'
+	},
+	robots: 'index, follow'
 };
+
+export const generateViewport = () => ({
+	width: 'device-width',
+	initialScale: 1.0
+});
 
 const RootLayout = async ({
 	children
