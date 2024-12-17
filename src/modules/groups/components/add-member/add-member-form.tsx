d43b4c9974f585d3d4bc287881d61129c0f928ad
Vit Nakladal <vit.nakladal@seacomp.cz>
2024-12-17 16:42:34 +0100

    fix: add member dialog fix?

--- src/modules/groups/components/add-member/add-member-form.tsx
+++ src/modules/groups/components/add-member/add-member-form.tsx
@@ -1,68 +1,52 @@
 'use client';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { FormProvider, useForm } from 'react-hook-form';
+import { type FormEvent, useRef } from 'react';
 import { toast } from 'sonner';
-import { z } from 'zod';
 
-import { FormTextField } from '@/components/form-fields/form-text-field';
-import { SubmitButton } from '@/components/submit-button';
+import { useAddGroupMemberMutation } from '../../hooks/create';
 
-import {
-	type AddGroupMemberMutationProps,
-	useAddGroupMemberMutation
-} from '../../hooks/create';
-
-const addMemberSchema = z.object({
-	userId: z.string(),
-	groupId: z.number()
-});
-
-type addMemberFormProps = {
-	groupId: number;
-};
-
-export const AddMemberForm = ({ groupId }: addMemberFormProps) => {
-	const methods = useForm<AddGroupMemberMutationProps>({
-		resolver: zodResolver(addMemberSchema),
-		defaultValues: {
-			groupId
-		}
-	});
-	const { handleSubmit } = methods;
-
+export const AddMemberForm = ({ groupId }: { groupId: number }) => {
+	const inputRef = useRef<HTMLInputElement>(null);
 	const mutation = useAddGroupMemberMutation();
-	const onSubmit = (data: AddGroupMemberMutationProps) => {
-		mutation.mutate(data, {
-			onSuccess: () => {
-				toast.success('User was successfully added to the group');
-			},
-			onError: error => {
-				toast.error(error.message);
-			}
-		});
+
+	const onSubmit = (e: FormEvent) => {
+		e.preventDefault();
+
+		const value = inputRef.current?.valueAsNumber;
+		console.log('ahoj value:', value, inputRef);
+
+		if (value) {
+			mutation.mutate(
+				{ userId: value, groupId },
+				{
+					onSuccess: () => {
+						toast.success('User was successfully added to the group');
+					},
+					onError: error => {
+						toast.error(error.message);
+					}
+				}
+			);
+		}
 	};
 
 	return (
-		<FormProvider {...methods}>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<FormTextField
-					name="userId"
-					label="User ID"
-					className="mb-3"
-					type="number"
-					labelClassName="text-sm font-medium text-neutral-600"
-					rows={1}
-				/>
-				<div className="flex items-center justify-end gap-2">
-					<SubmitButton
+		<form onSubmit={onSubmit} className="grow">
+			<div className="flex flex-col">
+				<div className="flex w-1/2 gap-x-2">
+					<input
+						ref={inputRef}
+						id="note-input"
+						className="grow rounded-md border-2 p-1.5"
+						type="text"
+					/>
+					<button
+						className="rounded-md border bg-white px-4 shadow-md"
 						type="submit"
-						className="my-4 w-full rounded-md bg-neutral-700 px-6 py-3 text-sm font-semibold text-white shadow-md transition-colors hover:bg-neutral-800 md:w-auto"
-						disabled={mutation.status === 'pending'}
 					>
-						{mutation.status === 'pending' ? 'Saving...' : 'Add user'}
-					</SubmitButton>
+						Add user
+					</button>
 				</div>
-			</form>
-		</FormProvider>
+			</div>
+		</form>
 	);
 };
--- src/modules/groups/hooks/create.ts
+++ src/modules/groups/hooks/create.ts
@@ -3,12 +3,12 @@ import { useMutation } from '@tanstack/react-query';
 import { createMembership } from '../server-actions/create';
 
 export type AddGroupMemberMutationProps = {
-	userId: string;
+	userId: number;
 	groupId: number;
 };
 
 export const useAddGroupMemberMutation = () =>
 	useMutation({
 		mutationFn: async ({ userId, groupId }: AddGroupMemberMutationProps) =>
-			createMembership(Number(userId), groupId)
+			createMembership(userId, groupId)
 	});
